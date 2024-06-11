83580268f28317a014469744ecf08202998ce5ca
codexpression-nj <n.codexpression@gmail.com>
2024-06-11 23:16:17 +0200

    auth service

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+// import { Auth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, UserCredential } from '@angular/fire/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+}
