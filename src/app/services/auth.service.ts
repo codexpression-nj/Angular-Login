b5bf443cacb4b0b74417f05311dc0c10dbae6bb8
codexpression-nj <n.codexpression@gmail.com>
2024-06-12 23:59:35 +0200

    uopdate

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,10 +1,33 @@
 import { Injectable } from '@angular/core';
-// import { Auth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, UserCredential } from '@angular/fire/auth';
+import { Auth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, UserCredential } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private auth: Auth) { }
+  async signUp(email: string, password: string): Promise<UserCredential> {
+    try {
+      return await createUserWithEmailAndPassword(this.auth, email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async signIn(email: string, password: string): Promise<UserCredential> {
+    try {
+      return await signInWithEmailAndPassword(this.auth, email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async signOut(): Promise<void> {
+    try {
+      return await signOut(this.auth);
+    } catch (error) {
+      throw error;
+    }
+  }
 }
